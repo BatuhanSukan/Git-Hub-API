d03b6c6d5c5f687b88958db26894ed005ce571fc
BatuhanSukan <48777954+BatuhanSukan@users.noreply.github.com>
2021-03-22 14:37:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// Elementleri seçme 
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github();
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners(){
+
+    githubForm.addEventListener("submit",getData);
+    clearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched);
+
+} 
+
+function getData(e){
+    let username = nameInput.value.trim();
+
+    if (username === ""){
+        alert("Lütfen geçerli bir kullanıcı adı girin");
+    }
+    else {
+        github.getGithubData(username)
+        .then(response => {
+            if (response.user.message === "Not Found"){
+                // Hata Mesajı
+                ui.showError("Kullanıcı Bulunamadı");
+            }
+            else {
+                ui.addSearchedUserToUI(username);
+                Storage.addSearchedUserToStorage(username);
+                ui.showUserInfo(response.user);
+                ui.showRepoInfo(response.repo)
+            }
+        })
+        .catch(err => ui.showError(err));
+    }
+
+    ui.clearInput(); // Input Temizleme
+    e.prevenDefault();
+}
+
+function clearAllSearched(){
+    // Tüm arananları temizle
+    if (confirm("Emin misiniz ?")){
+        Storage.clearAllSearchedUsersFromStorage(); //Storagedan Temizleme
+        ui.clearAllSearchedFromUI();
+
+
+    }
+
+}
+
+function getAllSearched(){
+    // Aranları Storagedan al ve Uİye yükle
+
+    let users = Storage.getSearchedUsersFromStorage();
+
+    let result = "";
+    users.forEach(user => {
+
+        result += `<li class="list-group-item">${user}</li>`;
+    });
+
+    lastUsers.innerHTML = result;
+
+}
+
